e2986b067dcc84d1ffb51fba3dd87169dc485a10
specture258 <specture258@hanyang.ac.kr>
2025-05-14 15:22:28 +0900

    Feat: add asset data view

---
+++ src/main/java/hyu/dayPocket/service/ReceiptService.java
@@ -0,0 +1,22 @@
+package hyu.dayPocket.service;
+
+import hyu.dayPocket.domain.Member;
+import hyu.dayPocket.dto.TargetReceiptFiPointDto;
+import hyu.dayPocket.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReceiptService {
+
+    public final MemberRepository memberRepository;
+
+    public void setTargetReceiptFiPoint(Member member, Integer targetReceiptFiPoint) {
+        TargetReceiptFiPointDto targetReceiptFiPointDto = TargetReceiptFiPointDto.targetReceiptFiPointFrom(targetReceiptFiPoint);
+        member.updateTargetReceiptFiPoint(targetReceiptFiPoint);
+    }
+
+
+
+}
