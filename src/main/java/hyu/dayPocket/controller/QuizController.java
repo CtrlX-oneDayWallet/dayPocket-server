229c5dcab5ea0b27d17a869b5593e5fdf7282721
noturavrigk <ksm92038386@gmail.com>
2025-05-16 00:14:20 +0900

    Feat: 유저의 문제 정답 확인 로직 추가

--- src/main/java/hyu/dayPocket/controller/QuizController.java
+++ src/main/java/hyu/dayPocket/controller/QuizController.java
@@ -1,9 +1,15 @@
 package hyu.dayPocket.controller;
 
+import hyu.dayPocket.config.CustomUserDetails;
+import hyu.dayPocket.dto.MemberChosenAnswer;
 import hyu.dayPocket.dto.QuizProvidingDto;
 import hyu.dayPocket.service.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +24,8 @@ public class QuizController {
         return quizService.provideQuiz();
     }
 
-
+    @PostMapping("/submit/quiz")
+    public int checkClientAnswer(@RequestBody List<MemberChosenAnswer> dto, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return quizService.checkIfCorrectAnswers(dto, userDetails.getMember());
+    }
 }
--- src/main/java/hyu/dayPocket/repository/QuizRepository.java
+++ src/main/java/hyu/dayPocket/repository/QuizRepository.java
@@ -1,6 +1,7 @@
 package hyu.dayPocket.repository;
 
 import hyu.dayPocket.domain.Quiz;
+import hyu.dayPocket.dto.MemberChosenAnswer;
 import hyu.dayPocket.dto.QuizProvidingDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,7 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
             "FROM Quiz q WHERE q.id IN :ids")
     List<QuizProvidingDto> findAllByInIdSet(@Param("ids") Set<Long> ids);
 
+    @Query("SELECT new hyu.dayPocket.dto.MemberChosenAnswer(q.id, q.answer) " +
+            "FROM Quiz q WHERE q.id IN :ids")
+    List<MemberChosenAnswer> findAllAnswerByInIdSet(@Param("ids") Set<Long> ids);
 }
