4df23f8f5d51479ce956d0633b124a9f2f313512
noturavrigk <ksm92038386@gmail.com>
2025-05-15 15:40:12 +0900

    Fix: 로그인 유저 정보 가져오는 방식 변경

--- src/main/java/hyu/dayPocket/controller/BankAccountController.java
+++ src/main/java/hyu/dayPocket/controller/BankAccountController.java
@@ -1,14 +1,11 @@
 package hyu.dayPocket.controller;
 
 import hyu.dayPocket.config.CustomUserDetails;
-import hyu.dayPocket.domain.BankAccount;
 import hyu.dayPocket.domain.Member;
 import hyu.dayPocket.dto.BankAccountData;
 import hyu.dayPocket.service.BankAccountService;
-import hyu.dayPocket.utils.AuthUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BankAccountController {
     private final BankAccountService bankAccountService;
-    private final AuthUtils authUtils;
 
     @PostMapping("/bank/account")
-    public String addBankAccount(@RequestBody BankAccountData bankAccountData) {
-        CustomUserDetails userDetails = authUtils.getRequestMember();
+    public String addBankAccount(@RequestBody BankAccountData bankAccountData, @AuthenticationPrincipal CustomUserDetails userDetails) {
         Member member = userDetails.getMember();
 
         bankAccountService.saveAccount(member, bankAccountData.getBank(), bankAccountData.getPassword(), bankAccountData.getAccountNumber());
--- src/main/java/hyu/dayPocket/controller/PointController.java
+++ src/main/java/hyu/dayPocket/controller/PointController.java
@@ -4,9 +4,8 @@ import hyu.dayPocket.config.CustomUserDetails;
 import hyu.dayPocket.domain.Member;
 import hyu.dayPocket.dto.PointWithdrawData;
 import hyu.dayPocket.service.BankAccountService;
-import hyu.dayPocket.service.MemberService;
-import hyu.dayPocket.utils.AuthUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PointController {
     private final BankAccountService bankAccountService;
-    private final AuthUtils authUtils;
 
     @PostMapping("/point/withdraw")
-    public String withdraw(@RequestBody PointWithdrawData pointWithdrawData) {
-        CustomUserDetails userDetails = authUtils.getRequestMember();
+    public String withdraw(@RequestBody PointWithdrawData pointWithdrawData, @AuthenticationPrincipal CustomUserDetails userDetails) {
         Member member = userDetails.getMember();
 
         bankAccountService.withdraw(member, pointWithdrawData.getPoint(), pointWithdrawData.getAccountNumber());
