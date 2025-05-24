5de10b1360563ff2c72d9eee314233c08501d85c
noturavrigk <ksm92038386@gmail.com>
2025-05-25 00:48:48 +0900

    fix: 퀴즈 제공 dto 오류 수정

--- src/main/java/hyu/dayPocket/dto/QuizProvidingDto.java
+++ src/main/java/hyu/dayPocket/dto/QuizProvidingDto.java
@@ -1,8 +1,14 @@
 package hyu.dayPocket.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class QuizProvidingDto {
     private Long id;
     private String question;
