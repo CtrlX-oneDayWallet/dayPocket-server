47e0a9339ab54b788e0942e39df94bef4a36d881
specture258 <specture258@hanyang.ac.kr>
2025-06-04 21:39:14 +0900

    Fix: targetReceiptFiPoint db 반영 오류 수정

--- src/main/java/hyu/dayPocket/service/ReceiptService.java
+++ src/main/java/hyu/dayPocket/service/ReceiptService.java
@@ -3,6 +3,7 @@ package hyu.dayPocket.service;
 import hyu.dayPocket.domain.Member;
 import hyu.dayPocket.dto.TargetReceiptFiPointDto;
 import hyu.dayPocket.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,8 +16,8 @@ public class ReceiptService {
 
     @Transactional
     public void setTargetReceiptFiPoint(Member member, Integer targetReceiptFiPoint) {
-        TargetReceiptFiPointDto dto = TargetReceiptFiPointDto.targetReceiptFiPointFrom(targetReceiptFiPoint);
-        member.updateTargetReceiptFiPoint(dto.getTargetReceiptFiPoint());
+        Member loginMember = memberRepository.findById(member.getId()).orElseThrow(() -> new EntityNotFoundException("Member is not found"));
+        loginMember.updateTargetReceiptFiPoint(targetReceiptFiPoint);
     }
 
 
